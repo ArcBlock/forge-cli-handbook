669404724c34ff5b843728ebb99c2081b69034e1
wangshijun <wangshijun2010@gmail.com>
2019-09-30 15:11:54 +0800

    chore: add book layout

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const path = require('path');
+const { version } = require('./package.json');
+
+module.exports = {
+  plugins: [
+    {
+      resolve: require.resolve('@arcblock/gatsby-theme-docs'),
+      options: {
+        version: `v${version}`,
+        sourceDirs: [path.resolve(__dirname, 'src')],
+        siteMetadata: {
+          title: 'ArcBlock',
+          subtitle: 'Forge CLI',
+          description: 'Forge SDK Documentation',
+        },
+        algoliaSearch: {
+          enabled: process.env.NODE_ENV !== 'production',
+          appId: process.env.GATSBY_ALGOLIA_APP_ID,
+          adminKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
+          searchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+        },
+        navItems: [],
+        extraPlugins: [],
+      },
+    },
+  ],
+};
